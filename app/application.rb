4a1da78d55e7f0d8ebc07ee0e8d0815baf979350
Bernadette Davis <bsd231@nyu.edu>
2019-10-08 14:11:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,6 @@ class Application
 
       if @@items.include?(search_term)
         @@cart << search_term
-        
         resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
